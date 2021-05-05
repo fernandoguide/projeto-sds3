f58f31e06dc5a3cb5c8baa067befb520dbdac07f
fernandoguide <fernando-guide@hotmail.com>
2021-05-05 12:34:49 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import NavBar from './components/Navbar/index';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
 
 function App() {
   return (
-    <div>
-         <h1 className="text-primary">SALVE SDS3</h1> 
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Ola Mundo</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 

5d09d080522650a9215ce78fef10ff6eb72143e0
fernandoguide <fernando-guide@hotmail.com>
2021-05-05 09:01:18 -0300

    criando o projeto

---
+++ backend/src/test/java/br/com/fernando/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernando.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

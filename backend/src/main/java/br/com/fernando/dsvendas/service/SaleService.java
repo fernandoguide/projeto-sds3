494aca469d2cb64133b0f50297464c7135000b3d
fernandoguide <fernando-guide@hotmail.com>
2021-05-08 12:40:21 -0300

    Group by search

--- backend/src/main/java/br/com/fernando/dsvendas/service/SaleService.java
+++ backend/src/main/java/br/com/fernando/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package br.com.fernando.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.fernando.dsvendas.dto.SaleDTO;
+import br.com.fernando.dsvendas.dto.SaleSuccessDTO;
+import br.com.fernando.dsvendas.dto.SaleSumDTO;
 import br.com.fernando.dsvendas.entities.Sale;
 import br.com.fernando.dsvendas.repositories.SaleRepository;
 import br.com.fernando.dsvendas.repositories.SellerRepository;
@@ -27,5 +31,13 @@ public class SaleService {
 		return result.map(x -> new SaleDTO(x));
 	}
 
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
 
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+	}
 }
